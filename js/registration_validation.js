f1168289c948a758787a33feeed0ae8774b84871
Kevin Vu <hongkhanhvu94@gmail.com>
2024-08-25 07:44:25 +1000

    add success message if the user fill in registration form with no error

--- js/registration_validation.js
+++ js/registration_validation.js
@@ -3,97 +3,109 @@ function registrationValidation() {
 
   // validate name field
   let name = registrationForm.elements["name"].value;
-  let nameValidResult = validateName(name);
-  console.log(nameValidResult);
+  let validName = isNameValid(name);
 
   // validate age field
   let age = registrationForm.elements["age"].value;
-  let ageValidResult = validateAge(age);
-  console.log(ageValidResult);
+  let validAge = isAgeValid(age);
 
   // validate email field
   let email = registrationForm.elements["email"].value;
-  let emailValidResult = validateEmail(email);
-  console.log(emailValidResult);
+  let validEmail = isEmailValid(email);
 
   // validate phone number
   let phone = registrationForm.elements["phone"].value;
-  let phoneValidResult = validatePhone(phone);
-  console.log(phoneValidResult);
+  let validPhone = isPhoneValid(phone);
+
+  let successMsg = document.getElementById("successMsg");
+  if (validName && validAge && validEmail && validPhone) {
+    let p = document.createElement("p");
+    let text = document.createTextNode("Registration is successful! Our team will be in contact with you shortly.");
+    p.appendChild(text);
+    successMsg.appendChild(p);
+    successMsg.style.color = "green";
+    successMsg.style.fontWeight = "bold";
+    successMsg.classList.remove("hidden");
+  }
 }
 
 function addError(field, message) {
-  var p = document.createElement("p");
-  var text = document.createTextNode(message);
+  successMsg.classList.add("hidden");
+  let p = document.createElement("p");
+  let text = document.createTextNode(message);
   p.appendChild(text);
   let id = "error" + field;
   document.getElementById(id).innerHTML = "";
   document.getElementById(id).appendChild(p);
 }
 
-function validateName(name) {
+function isNameValid(name) {
   document.getElementById("errorname").innerHTML = "";
   if (!name) {
     addError("name", "Name field must not be empty.");
-    return;
+    return false;
   }
 
   let regName = /^[ a-zA-Z\-\’,]+$/;
   if (!regName.test(name)) {
     addError("name", "Invalid name given.");
-    return;
+    return false;
   }
+  return true;
 }
 
-function validateAge(age) {
+function isAgeValid(age) {
   document.getElementById("errorage").innerHTML = "";
   if (!age) {
     addError("age", "Age field must not be empty.");
-    return;
+    return false;
   }
 
   age = parseFloat(age);
 
   if (!Number.isInteger(age)) {
     addError("age", "Age must be an integer (whole number).");
-    return;
+    return false;
   }
 
   if (age < 18 || age > 130) {
     addError("age", "Age must be a number between 18 and 130.");
-    return;
+    return false;
   }
+  return true;
 }
 
-function validateEmail(email) {
+function isEmailValid(email) {
   document.getElementById("erroremail").innerHTML = "";
   if (!email) {
     addError("email", "Email field must not be empty.");
-    return;
+    return false;
   }
 
   let regEmail = /^[a-zA-Z-]([\w-.]+)?@([\w-]+\.)+[\w]+$/;
   if (!regEmail.test(email)) {
     addError("email", "Invalid email given.");
-    return;
+    return false;
   }
+  return true;
 }
 
-function validatePhone(phone) {
+function isPhoneValid(phone) {
   document.getElementById("errorphone").innerHTML = "";
   if (phone) {
     if (!/^\d+$/.test(phone)) {
       addError("phone", "Phone number must contain only digits (no letters or symbols).");
-      return;
+      return false;
     } else if (phone.length !== 10) {
       addError("phone", "Your phone number must be 10 digits long.");
-      return;
+      return false;
     } else if (!/^[0-9]+$/.test(phone)) {
       addError("phone", "Your phone number must only contain the digits 0-9.");
-      return;
+      return false;
     } else if (!/^04/.test(phone)) {
       addError("phone", "Your phone number must begin with 04.");
-      return;
+      return false;
     }
   }
+  return true;
 }
